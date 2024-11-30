d02bfe635945fa7ed16cff7b45e877d97ba9ea23
Nathan Schultz <nathan.r.a.schultz@gmail.com>
2024-11-30 00:19:16 -0500

    Further improvements to home page

--- eegscape/src/pages/Home.jsx
+++ eegscape/src/pages/Home.jsx
@@ -1,23 +1,18 @@
+import { useState } from "react";
+import Welcome from "../components/Welcome";
+import Menu from "../components/Menu";
+
 function Home() {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleContinue = () => {
+    setShowMenu(true);
+  };
+
   return (
-    <div className="flex flex-col items-center space-y-8 p-8">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4">Welcome!</h1>
-      </div>
-
-      <div className="space-y-4">
-        <p>This line will have instructions on connecting the headset</p>
-        <p>
-          This line will instruct the user how to proceed to click the button
-          once the headset is connected
-        </p>
-      </div>
-
-      <div>
-        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-          Continue
-        </button>
-      </div>
+    <div>
+      {showMenu ? <Menu /> : <Welcome onContinue={handleContinue} />}
     </div>
   );
 }
