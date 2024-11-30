b3c43edf26a13b99d218c715cd825acc171d091d
Mahdi Raza Khunt <82615934+mrkhunt@users.noreply.github.com>
2024-11-29 23:21:27 -0500

    fix: add inputs testing router

--- eegscape/src/App.jsx
+++ eegscape/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import Inputs from "./inputs/Inputs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/inputs" element={<Inputs />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ eegscape/src/inputs/Inputs.jsx
@@ -0,0 +1,6 @@
+function Inputs() {
+    return <div className="font-bold text-red-500 btn btn-primary">Inputs</div>;
+  }
+  
+  export default Inputs;
+  
