5d1d122afd34631290951f18efdec5e24cfd3389
Mahdi Raza Khunt <82615934+mrkhunt@users.noreply.github.com>
2024-12-01 01:32:07 -0500

    fix: bad bug

--- eegscape/src/hooks/useReceiveEeg.js
+++ eegscape/src/hooks/useReceiveEeg.js
@@ -24,7 +24,7 @@ function useReceiveEeg() {
   };
 
   const blink = {
-    useBlink: (handler) => useEventSubscriber(EEG_EVENT_CODES.yaw, handler),
+    useBlink: (handler) => useEventSubscriber(EEG_EVENT_CODES.blink, handler),
   };
 
   return {
